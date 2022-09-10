62025f2a0aedde59d86c3058157aaf42997c7551
Hendra Manudinata <109848194+hdrmaa@users.noreply.github.com>
2022-09-10 20:26:47 +0700

    [TEST] Update WSGI script

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-from starterPack import create_app
-import sys
-
 if __name__ == "__main__":
+    import sys
     path = "/var/www/webroot/ROOT"
     if path not in sys.path:
         sys.path.append(path)
+
+    from starterPack import create_app
     app = create_app()
     app.run()
